b815c16b5b6d7b5f2bbdcd5269bec787552172bc
VINAYKUMARKUNDER <vinay3000111@gmail.com>
2023-04-22 19:43:31 +0530

    fronted

--- ph-app/fronted/src/app/app-routing.module.ts
+++ ph-app/fronted/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceComponent } from './service/service.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:ServiceComponent,
+    path:'service'
+  },
+  {
+    component:HomePageComponent,
+    path:''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
