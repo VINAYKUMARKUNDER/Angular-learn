e033056b5e1bdb3389cb1791e716f09c36067198
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-06 12:22:10 +0530

    tool module created

--- ph-app/backend/routers/ToolController.js
+++ ph-app/backend/routers/ToolController.js
@@ -2,6 +2,29 @@ const express = require("express");
 const router = express.Router();
 
 const db = require("../database");
+const Tool = require('../models/ToolModule')
+
+// get all tools
+router.get('/', async (req, res)=>{
+    try {
+        const allTool = await  Tool.findAll();
+       return res.status(200).json(allTool);
+    } catch (error) {
+        return res.status(500).json('Internal server error...')
+    }
+      
+});
+
+
+// created new tool
+router.post('/', async (req, res)=>{
+    try {
+        const createdData= await Tool.create(req.body);
+        return res.status(201).json(createdData);
+    } catch (error) {
+        return res.status(500).json('Internal server error...');
+    }
+});
 
 
 
