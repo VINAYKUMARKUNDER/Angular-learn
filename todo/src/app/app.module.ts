277ce6e6269cf97fe533769fccb4c70251445f80
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-21 12:34:29 +0530

    add navbar

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MyTodoComponent } from './component/my-todo/my-todo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyTodoComponent
+    MyTodoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
